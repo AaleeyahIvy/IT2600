07baf205acd8f7f8d710387587772fcc2d23a88e
Aaleeyah Ivy <53322518+AaleeyahIvy@users.noreply.github.com>
2022-02-21 12:22:27 -0500

    Add files via upload

---
+++ ch05_ex1/view/categories_nav.php
@@ -0,0 +1,11 @@
+<nav>
+        <ul>
+        <?php foreach ($categories as $category) : ?>
+            <li>
+            <a href="?category_id=<?php echo $category['categoryID']; ?>">
+                <?php echo $category['categoryName']; ?>
+            </a>
+            </li>
+        <?php endforeach; ?>
+        </ul>
+        </nav>
